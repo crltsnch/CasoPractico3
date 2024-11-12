af55ed961c255c824c38c182ca138c929aee6ffa
crltsnch <crltsnch@myuax.com>
2024-11-12 18:44:10 +0100

    LSTM predicciones

--- IBEX/modelos/LSTM_script.py
+++ IBEX/modelos/LSTM_script.py
@@ -8,56 +8,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.models import load_model
 
-# Cargar los datos desde el archivo CSV
-file_path = "IBEX/data/ibex_data_clean.csv"
-df = pd.read_csv(file_path)
-
-# Mostrar las primeras filas del DataFrame en Streamlit
-st.write("Datos originales:")
-st.write(df.head())
-
-# Convertir la columna 'Date' a tipo datetime y establecerla como índice
-df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-df.set_index('Date', inplace=True)
-
-# Filtrar todas las filas donde 'Close' no sea nulo o NaN
-df_filtered = df.dropna(subset=['Close'])
-
-# Seleccionar la columna objetivo
-data = df_filtered[['Close']]
-
-# Escalar los datos entre 0 y 1
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(data)
-
-# Crear secuencias de datos para entrenamiento
-def create_sequences(data, seq_length):
-    X, y = [], []
-    for i in range(len(data) - seq_length):
-        X.append(data[i:i + seq_length])
-        y.append(data[i + seq_length])
-    return np.array(X), np.array(y)
-
-# Definir la longitud de la secuencia
-seq_length = 365
-X, y = create_sequences(scaled_data, seq_length)
-
-# Dividir los datos en entrenamiento y prueba (80% - 20%)
-train_size = int(len(X) * 0.8)
-X_train, X_test = X[:train_size], X[train_size:]
-y_train, y_test = y[:train_size], y[train_size:]
-
-# Construir el modelo LSTM
-model = Sequential()
-model.add(LSTM(128, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
-model.add(LSTM(64))
-model.add(Dense(1))
-
-# Compilar el modelo
-model.compile(optimizer='adam', loss='mean_squared_error')
-
-# Entrenar el modelo
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+# Cargar el modelo entrenado
+model = load_model("modelo_lstm_ibex.h5")
 
 # Evaluar el modelo
 loss = model.evaluate(X_test, y_test)
@@ -148,16 +100,10 @@ r2_train = r2_score(scaler.inverse_transform(y_train), train_predictions_unscale
 st.write(f'Coeficiente de determinación R² en entrenamiento: {r2_train:.2f}')
 st.write(f'Coeficiente de determinación R² en prueba: {lstm_metrics[3]:.2f}')
 
-# Guardar modelo
-save_path = "modelo_lstm_ibex.h5"
-model.save(save_path)
-st.write(f"Modelo guardado en: {save_path}")
 
-# Cargar el modelo entrenado
-model = load_model("modelo_lstm_ibex.h5")
 
 # Número de días a predecir (un mes completo)
-prediction_days = 30
+prediction_days = 7
 
 # Crear una lista para almacenar las predicciones
 predictions = []
