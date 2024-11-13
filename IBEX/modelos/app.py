0e709b3c1dc77d2d13eecc9c0165cad1c38eeaf3
crltsnch <91721777+crltsnch@users.noreply.github.com>
2024-11-13 15:16:16 +0000

    creando streamlit

--- IBEX/modelos/app.py
+++ IBEX/modelos/app.py
@@ -21,104 +21,12 @@ std_residuals = residuals.std()
 confidence_interval_upper = results['Predicted'] + std_residuals
 confidence_interval_lower = results['Predicted'] - std_residuals
 
-st.write("### Gráfico de Predicciones con Intervalo de Confianza del 70%")
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(results['Date'], results['Real'], label='Valores Reales', color='blue')
-ax.plot(results['Date'], results['Predicted'], label='Predicciones LSTM', linestyle='--', color='red')
-ax.fill_between(results['Date'], confidence_interval_lower, confidence_interval_upper, color='gray', alpha=0.3, label='Intervalo de Confianza 70%')
-ax.set_xlabel('Fecha')
-ax.set_ylabel('Precio de Cierre')
-ax.set_title('Comparación de Valores Reales y Predicciones LSTM con Intervalo de Confianza')
-ax.legend()
-st.pyplot(fig)
-
-# Graficar residuos
-st.write("### Gráfico de Residuos")
-fig2, ax2 = plt.subplots(figsize=(12, 6))
-ax2.plot(results['Date'], residuals, label='Residuos', color='red')
-ax2.axhline(y=0, color='black', linestyle='--', linewidth=0.5)
-ax2.set_xlabel('Fecha')
-ax2.set_ylabel('Residuos')
-ax2.set_title('Residuos del Modelo LSTM')
-ax2.legend()
-
-# Mostrar el gráfico de residuos
-st.pyplot(fig2)
-
-# Mostrar histograma de residuos
-st.write("### Histograma de Residuos")
-fig3, ax3 = plt.subplots(figsize=(12, 6))
-ax3.hist(residuals, bins=20, color='red', alpha=0.5)
-ax3.set_xlabel('Residuos')
-ax3.set_ylabel('Frecuencia')
-ax3.set_title('Histograma de Residuos del Modelo LSTM')
-st.pyplot(fig3)
-
-
-
-st.write("### Predicciones para los próximos 5 días del IBEX35")
-
-# Convertir a datetime
-future_predictions.index = pd.to_datetime(future_predictions.index)
-
-# Filtrar los últimos 30 días de los datos reales y las predicciones
-last_30_days_data = results['Real'].iloc[-30:]
-last_30_days_predictions = future_predictions['Predicted Close'].iloc[:30]  # Selecciona las primeras 30 predicciones
-
-# Graficar el zoom de las predicciones de los últimos 30 días
-fig5, ax5 = plt.subplots(figsize=(14, 7))
-
-# Datos reales (últimos 30 días)
-ax5.plot(results['Date'].iloc[-30:], last_30_days_data, label='Datos de los últimos 30 Días)', color='blue')
-
-# Predicciones LSTM (últimos 30 días)
-ax5.plot(future_predictions.index[:30], last_30_days_predictions, label='Predicciones LSTM (Próximos 5 Días)', color='red', linestyle='--')
-
-# Conectar el último dato real con el primer dato predicho con una línea discontinua roja
-last_real_date = results['Date'].iloc[-1]
-first_predicted_date = future_predictions.index[0]
-first_predicted_value = future_predictions['Predicted Close'].iloc[0]
-
-# Graficar la línea discontinua que conecta el último valor real con el primero predicho
-ax5.plot([last_real_date, first_predicted_date], [last_30_days_data.iloc[-1], first_predicted_value], color='red', linestyle='--')
-
-# Etiquetas y título
-ax5.set_xlabel('Fecha')
-ax5.set_ylabel('Precio de Cierre')
-ax5.set_title('Predicciones para los Próximos 5 Días')
-ax5.legend()
-
-# Mostrar el gráfico en Streamlit
-st.pyplot(fig5)
-
-
-
-
-# Suponiendo que 'future_predictions' ya tiene los datos que mencionaste
-future_predictions['Fecha'] = future_predictions.index.strftime('%d %b %Y').str.lower()
-
-# Restablecer el índice y eliminar la columna del índice
-future_predictions = future_predictions.reset_index(drop=True)
-
-# Renombrar las columnas para mayor claridad
-future_predictions = future_predictions.rename(columns={
-    'Predicted Close': 'Predicción de Cierre',
-    'Price Change': 'Cambio',
-    'Percentage Change': 'Cambio Porcentual (%)'
-})
-
-# Establecer 'Fecha' como índice
-future_predictions.set_index('Fecha', inplace=True)
-
 # Función para redondear y eliminar ceros innecesarios
 def format_column(val):
     if isinstance(val, (int, float)):
         # Redondear a 2 decimales y eliminar ceros innecesarios
         return f'{val:.2f}'.rstrip('0').rstrip('.')
     return val
-
-# Aplicar la función de formato a todas las columnas
-future_predictions = future_predictions.applymap(format_column)
 
 # Estilo para cambiar el color según el valor
 def color_change(val):
@@ -130,10 +38,190 @@ def color_change(val):
     except:
         return ''  # Para valores que no sean numéricos, no aplicar color
 
-# Aplicar el estilo a las columnas 'Cambio' y 'Cambio Porcentual (%)'
-styled_df = future_predictions.style.applymap(color_change, subset=['Cambio', 'Cambio Porcentual (%)'])
+st.set_page_config(layout="wide")
 
-# Mostrar la tabla estilizada
-st.write(styled_df)
+# Encabezado de la página
+st.markdown(
+    """
+    <div style="
+        background-color: #424863;
+        padding: 15px;
+        text-align: center;
+        border-radius: 8px;
+        color: white;
+        font-size: 24px;
+        font-weight: bold;
+        margin-bottom: 50px;
+        ">
+        Predicciones IBEX35 con LSTM
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Menú de navegación superior
+page = st.selectbox("Selecciona una página", ["Predicciones Futuras", "Nuestro Análisis"], index=0)
+
+# Página 1: Nuestro Análisis
+if page == "Nuestro Análisis":
+    st.write("### Nuestro modelo de Predicción")
+
+    # Crear primera fila de gráficos con dos columnas
+    col1, col2 = st.columns([1, 1])
+
+    with col1:
+        # Segunda columna vacía
+        st.write("")
+        
+
+    with col2:
+        # Gráfico de Predicciones con Intervalo de Confianza
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.plot(results['Date'], results['Real'], label='Valores Reales', color='blue')
+        ax.plot(results['Date'], results['Predicted'], label='Predicciones LSTM', linestyle='--', color='red')
+        ax.fill_between(results['Date'], confidence_interval_lower, confidence_interval_upper, color='gray', alpha=0.3, label='Intervalo de Confianza 70%')
+        ax.set_xlabel('Fecha', fontsize=12)  # Ajuste del tamaño de la etiqueta del eje x
+        ax.set_ylabel('Precio de Cierre', fontsize=12)  # Ajuste del tamaño de la etiqueta del eje y
+        ax.set_title('Comparación de Valores Reales y Predicciones LSTM', fontsize=10)  # Título más grande
+        ax.tick_params(axis='x', labelsize=10)  # Tamaño de fuente de los valores del eje x
+        ax.tick_params(axis='y', labelsize=10)  # Tamaño de fuente de los valores del eje y
+        ax.legend(fontsize=10)  # Tamaño de fuente de la leyenda
+        st.pyplot(fig)
+        
+
+    # Espacio en blanco entre las filas
+    st.markdown("<br><hr><br>", unsafe_allow_html=True)
+
+    # Crear segunda fila de gráficos con una columna y un espacio vacío
+    col3, col4 = st.columns([1, 1])
+
+    with col3:
+        # Gráfico de Residuos
+        fig2, ax2 = plt.subplots(figsize=(10, 6))
+        ax2.plot(results['Date'], residuals, label='Residuos', color='red')
+        ax2.axhline(y=0, color='black', linestyle='--', linewidth=0.5)
+        ax2.set_xlabel('Fecha', fontsize=12)  # Ajuste del tamaño de la etiqueta del eje x
+        ax2.set_ylabel('Residuos', fontsize=12)  # Ajuste del tamaño de la etiqueta del eje y
+        ax2.set_title('Residuos del Modelo LSTM', fontsize=10)  # Título más grande
+        ax2.tick_params(axis='x', labelsize=10)  # Tamaño de fuente de los valores del eje x
+        ax2.tick_params(axis='y', labelsize=10)  # Tamaño de fuente de los valores del eje y
+        ax2.legend(fontsize=10)  # Tamaño de fuente de la leyenda
+        st.pyplot(fig2)
+    
+    with col4:
+        # Histograma de Residuos
+        fig3, ax3 = plt.subplots(figsize=(10, 6))
+        ax3.hist(residuals, bins=20, color='red', alpha=0.5)
+        ax3.set_xlabel('Residuos', fontsize=10)  # Ajuste del tamaño de la etiqueta del eje x
+        ax3.set_ylabel('Frecuencia', fontsize=10)  # Ajuste del tamaño de la etiqueta del eje y
+        ax3.set_title('Histograma de Residuos del Modelo LSTM', fontsize=10)  # Título más grande
+        ax3.tick_params(axis='x', labelsize=12)  # Tamaño de fuente de los valores del eje x
+        ax3.tick_params(axis='y', labelsize=12)  # Tamaño de fuente de los valores del eje y
+        ax3.grid(True)
+        st.pyplot(fig3)
+        
+
+
+
+# Página 2: Predicciones Futuras
+if page == "Predicciones Futuras":
+    st.write("### Predicciones para los próximos 5 días del IBEX35")
+
+    # Convertir a datetime
+    future_predictions.index = pd.to_datetime(future_predictions.index)
+
+    # Filtrar los últimos 30 días de los datos reales y las predicciones
+    last_30_days_data = results['Real'].iloc[-30:]
+    last_30_days_predictions = future_predictions['Predicted Close'].iloc[:30]  # Selecciona las primeras 30 predicciones
+
+    # Graficar el zoom de las predicciones de los últimos 30 días
+    fig5, ax5 = plt.subplots(figsize=(18, 8))
+
+    # Datos reales (últimos 30 días)
+    ax5.plot(results['Date'].iloc[-30:], last_30_days_data, label='Datos de los últimos 30 Días)', color='blue')
+
+    # Predicciones LSTM (últimos 30 días)
+    ax5.plot(future_predictions.index[:30], last_30_days_predictions, label='Predicciones LSTM (Próximos 5 Días)', color='red', linestyle='--')
+
+    # Conectar el último dato real con el primer dato predicho con una línea discontinua roja
+    last_real_date = results['Date'].iloc[-1]
+    first_predicted_date = future_predictions.index[0]
+    first_predicted_value = future_predictions['Predicted Close'].iloc[0]
+
+    # Graficar la línea discontinua que conecta el último valor real con el primero predicho
+    ax5.plot([last_real_date, first_predicted_date], [last_30_days_data.iloc[-1], first_predicted_value], color='red', linestyle='--')
+
+    # Etiquetas y título
+    ax5.set_xlabel('Fecha')
+    ax5.set_ylabel('Precio de Cierre')
+    ax5.set_title('Predicciones para los Próximos 5 Días')
+    ax5.legend()
+
+    # Mostrar el gráfico en Streamlit
+    st.pyplot(fig5)
+
+    # Preparar y mostrar la tabla estilizada
+    future_predictions['Fecha'] = future_predictions.index.strftime('%d %b %Y').str.lower()
+
+    # Restablecer el índice y eliminar la columna del índice
+    future_predictions = future_predictions.reset_index(drop=True)
+
+    # Renombrar las columnas para mayor claridad
+    future_predictions = future_predictions.rename(columns={
+        'Predicted Close': 'Predicción de Cierre',
+        'Price Change': 'Cambio',
+        'Percentage Change': 'Cambio Porcentual (%)'
+    })
+
+    # Establecer 'Fecha' como índice
+    future_predictions.set_index('Fecha', inplace=True)
+
+    # Aplicar la función de formato a todas las columnas
+    future_predictions = future_predictions.applymap(format_column)
+
+    # Mostrar predicciones como tarjetas en un diseño de 5 columnas
+    st.markdown("<h3 style='text-align: center;'>Detalles de las Predicciones</h3>", unsafe_allow_html=True)
+
+    # Crear columnas para las tarjetas
+    cols = st.columns(5)
+
+    # Iterar a través de las filas y mostrar cada predicción en las columnas
+    for i, (index, row) in enumerate(future_predictions.iterrows()):
+        cambio_color = 'red' if float(row['Cambio']) < 0 else 'green'
+        cambio_porcentual_color = 'red' if float(row['Cambio Porcentual (%)']) < 0 else 'green'
+
+        # Distribuir las tarjetas en 5 columnas
+        with cols[i % 5]:
+            st.markdown(
+                f"""
+                <div style="
+                    border: 1px solid #ddd;
+                    border-radius: 10px;
+                    padding: 20px;
+                    margin-bottom: 10px;
+                    background-color: #f9f9f9;
+                    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.1);
+                    width: 100%;
+                ">
+                    <h4 style="margin: 0; color: #424863; font-size: 18px;">{index}</h4>
+                    <p style="margin: 5px 0; font-size: 18px;">
+                        <strong>Cierre:</strong> {row['Predicción de Cierre']} €
+                    </p>
+                    <p style="margin: 5px 0; font-size: 18px; color: {cambio_color};">
+                        <strong>
+                            { '↑' if float(row['Cambio']) > 0 else '↓' }
+                            {row['Cambio']} €
+                            ({row['Cambio Porcentual (%)']}%)
+                        </strong>
+                    </p>
+
+
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+
+
+
 
 
